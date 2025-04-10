c233a5ecc05e65296d6ac8a56ae122ad5a6f8990
Pablo_Ruiz_Z <paporuiz08@gmail.com>
2025-04-09 23:26:39 -0600

    Add project files.

---
+++ minsat/Factura.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace minsat
+{
+    class Factura
+    {
+
+        public double SubTotal = -1;
+        public double Total = -1;
+        public string concepto = null;
+        public string fecha = null;
+
+
+        public string toString()
+        {
+            return $"|{concepto}|{fecha}|{SubTotal}|{Total}";
+        }
+    }
+}
---
+++ minsat/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using minsat;
+using System.IO;
+using System.Xml;
+using Spectre.Console;
+
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        // Handle command-line arguments here
+
+
+        AnsiConsole.Markup("[underline red]Welcome to Minisat[/] ");
+        //if no directory is specified, choosew whatever directory the console is on
+        DirectoryInfo d = new DirectoryInfo(args[0] == null ? "./" : args[0]);
+        FileInfo[] Files = d.GetFiles(); //Getting Text files
+        XmlDocument doc = new XmlDocument();
+
+        double total_ant_imps = 0;
+        double total = 0;
+        List<Factura> Elementos = new List<Factura>();
+        for (int i = 0; i < Files.Length; i++)
+        {
+
+            FileInfo f = Files[i];
+            doc.Load(f.DirectoryName + '/' + f.Name);
+            int node_num = 0;
+            XmlNode node = doc.ChildNodes[node_num];           // second child
+            while (node.Name != "cfdi:Comprobante")
+            {
+                node_num += 1;
+                node = doc.ChildNodes[node_num];
+            }
+
+
+            Factura elem = new Factura();
+            elem.SubTotal = double.Parse(node.Attributes["SubTotal"]?.InnerText);
+            elem.Total = double.Parse(node.Attributes["Total"]?.InnerText);
+            elem.fecha = node.Attributes["Fecha"]?.InnerText;
+            int x = 1;
+            while (doc.ChildNodes[node_num].ChildNodes[x].Name != "cfdi:Conceptos")
+            {
+                x += 1;
+                node = doc.ChildNodes[node_num].ChildNodes[x];
+            }
+
+            elem.concepto = node.Attributes["Descripcion"]?.InnerText;
+
+            total_ant_imps += elem.SubTotal;
+            total += elem.Total;
+            Elementos.Add(elem);
+        }
+
+        Console.WriteLine("Subtotales: " + total_ant_imps);
+        Console.WriteLine("Totales: " + total);
+        Console.WriteLine(Elementos.ToString());
+
+    }
+}
+
+
+
+
+
